a9ef80bc151978536f1617d674105cb0ece11913
Peace <s.cihan.altinsoy@gmail.com>
2024-03-30 15:12:42 +0300

    Added Array Question 1

---
+++ Arrays/1-C Program to Print a 2D Array.c
@@ -0,0 +1,14 @@
+//C Program to Print a 2D Array
+#include <stdio.h>
+
+int main(){
+
+    int arr[5] = {1,2,3,4,5};
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d\n", i[arr]);
+    }
+    
+
+}
