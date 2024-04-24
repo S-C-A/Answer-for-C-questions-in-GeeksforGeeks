38117c65d01c7c987d17a16c879caea8bd75f49c
Peace <s.cihan.altinsoy@gmail.com>
2024-04-24 18:09:57 +0300

    Added Array Question 19

--- Arrays/19-C Program to Check Whether Two Matrices Are Equal or Not.c
+++ Arrays/19-C Program to Check Whether Two Matrices Are Equal or Not.c
@@ -1,44 +1,50 @@
-#include <stdio.h>
+#include <stdio.h> 
 
-void equal(int[][2], int[][2]);
+int areSame(int A[][4], int B[][4]) 
+{ 
+    int i, j; 
+    for (i = 0; i < 4; i++) 
+        for (j = 0; j < 4; j++) 
+            if (A[i][j] != B[i][j]) 
+                return 0; 
+    return 1; 
+} 
+  
 
-int main(){
+int main() 
+{ 
+  
 
-    int arr1[2][2], arr2[2][2];
-
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            scanf("%d", &arr1[i][j]);
-        }
-    }
-
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            scanf("%d", &arr2[i][j]);
-        }
-    }
-     
-    equal(arr1,arr2);
-}
-
-void equal(int arr1[][2], int arr2[][2]){
-
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            if (arr1[i][j] != arr2[i][j])
-            {
-                printf("The matricies aren't equal");
-                return;
-            }
-            
-        }
-    }
-    printf("matricies are equal");
-    return;
+    int A[4][4] = { { 1, 2, 3, 4 }, 
+                    { 1, 2, 3, 4 }, 
+                    { 1, 2, 3, 4 }, 
+                    { 1, 2, 3, 4 } }; 
+  
+    int B[4][4] = { { 1, 2, 3, 4 }, 
+                    { 1, 2, 3, 4 }, 
+                    { 1, 2, 3, 4 }, 
+                    { 1, 2, 3, 4 } }; 
+  
+ 
+    printf("\n First Matrix \n"); 
+    for (int i = 0; i < 4; i++) { 
+        for (int j = 0; j < 4; j++) { 
+            printf(" %d", A[i][j]); 
+        } 
+        printf("\n"); 
+    } 
+  
+    printf("\n Second Matrix \n"); 
+    for (int i = 0; i < 4; i++) { 
+        for (int j = 0; j < 4; j++) { 
+            printf(" %d", B[i][j]); 
+        } 
+        printf("\n"); 
+    } 
+  
+    if (areSame(A, B)) 
+        printf("\n Matrices are equal"); 
+    else
+        printf("\n Matrices are not equal"); 
+    return 0; 
 }
