dd295af9736dd385bef8e4a77e3829b3a141ed60
Peace <s.cihan.altinsoy@gmail.com>
2024-04-26 18:33:48 +0300

    Added Array Question 10

---
+++ Arrays/10-C Program to Sort the Elements of an Array in Descending Order.c
@@ -0,0 +1,49 @@
+//C Program to Sort the Elements of an Array in Descending Order
+//I used selection sort but anything is fine really
+#include <stdio.h>
+
+void selection_sort(int[], int);
+
+int main(){
+
+    int arr[] = { 45, 22, 100, 66, 37 }; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+
+    printf("The array at the start \n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+
+    selection_sort(arr, n);
+
+    printf("The sorted array \n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ",arr[i]);
+    }   
+}
+
+void selection_sort(int arr[], int size){
+
+    int temp, swap, temp_loc;
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        temp = arr[i];
+        temp_loc = i;
+
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] > temp)
+            {
+                temp = arr[j];
+                temp_loc = j;
+            }           
+        }
+        swap = arr[i];
+        arr[i] = temp;
+        arr[temp_loc] = swap;
+    }
+}
